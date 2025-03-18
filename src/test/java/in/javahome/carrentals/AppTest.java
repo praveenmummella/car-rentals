72b7e2fda76755075e3301b2d69d0a5b091488db
praveenmummella <praveen@PraveenMummella>
2025-03-18 14:56:38 +0530

    Added AppTest.java for unit testing

---
+++ src/test/java/in/javahome/carrentals/AppTest.java
@@ -0,0 +1,11 @@
+package in.javahome.carrentals;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
