4e2a66fac9985fd2e8039e74897ab3459c4a3aab
praveenmummella <80162116+praveenmummella@users.noreply.github.com>
2025-03-28 17:30:02 +0530

    Update DummyTest.java

--- src/main/java/com/example/DummyTest.java
+++ src/main/java/com/example/DummyTest.java
@@ -1,11 +1,10 @@
-package com.example;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+public class DummyTest {
 
-class DummyTest {
     @Test
-    void sampleTest() {
+    public void sampleTest() {
         assertTrue(true);
     }
 }
